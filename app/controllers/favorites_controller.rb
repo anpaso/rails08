50995e5debbd6ceb3c182e0cb990cdd564ac89b6
岩田和記 <ik@iwatakazunorinoMacBook-ea.local>
2018-11-04 19:39:45 +0900

    first commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,12 @@
 class FavoritesController < ApplicationController
 #require "pry"
 
-before_action :set_topic
+before_action :set_topic,except:[:index]
 
   def index
     @favorite_topics = current_user.favorite_topics
   end
+
   def create
     #binding.pry
     favorite = Favorite.new
@@ -18,21 +19,25 @@ before_action :set_topic
     else
       redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
     end
+    #render  json: favorite.user_id,favorite.topic_id
     #データベースからレコードを再取得する
     @topic.reload
   end
   def destroy
+    #binding.pry
     #変数likeにロード
     like = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
     #変数likeにロードしたオブジェクトを削除
     like.destroy
     #いいね数を表示するための@likes
-    @likes = Like.where(post_id: params[:post_id])
+    @likes = Favorite.where(topic_id: params[:topic_id])
     #データベースからレコードを再取得する
     @topic.reload
+    redirect_to topics_path
   end
+
   private
   def set_topic
-    @topic = Topic.find(params[user.favorite_users])
+    @topic = Topic.find(params[:topic_id])
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -10,14 +10,5 @@ class Topic < ApplicationRecord
   has_many :favorite_users, through: :favorites, source: 'user'
   has_many :comments
 
-# トピックをいいねする
-  def iine(user_id)
-    favorites.create(user_id: user_id)
-  end
-
-  # トピックのいいねを解除する
-  def uniine(user_id)
-    favorites.find_by(user_id: user_id).destroy
-  end
 
 end
